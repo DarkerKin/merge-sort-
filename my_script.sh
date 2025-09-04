10aa7c6874aaf3984991eb0600762dffa55c7472
astroworld <astroworld@astroworlds-MacBook-Air.local>
2025-09-04 16:30:02 -0400

    this is it

--- my_script.sh
+++ my_script.sh
@@ -1,15 +1,12 @@
 #! /bin/bash
 
+pwd
 
 # load python module if needed
 module load python
 
-# make a new venv (only once)
-python3 -m venv myenv
 
 # activate it
 source myenv/bin/activate
-
-pip install -r requirements.txt
 
 python3 benchmark.py
