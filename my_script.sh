2623f99fe1cd6d4d99ccc21ed5328396bd9885be
naem <you@example.com>
2025-09-06 23:18:03 -0400

    trying it again

--- my_script.sh
+++ my_script.sh
@@ -1,13 +1,20 @@
 #! /bin/bash
+#SBATCH --job-name=myjob
+#SBATCH --partition=Centaurus
+#SBATCH --time=00:30:00
+$HOME/myprogram
 
+#starting the process
 echo "starting process"
 
+#which directory
 pwd
 
 # load python module if needed
 module load python
 
-
+#run the program
 python3 benchmark.py
 
+#notify when done
 echo "done"
