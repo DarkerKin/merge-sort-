1bd87a8a08528837b2d137ccf4d63233404a5220
astroworld <astroworld@astroworlds-MacBook-Air.local>
2025-09-06 22:58:12 -0400

    gogo gaga

--- my_script.sh
+++ my_script.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
 
+echo "starting process"
+
 pwd
 
 # load python module if needed
 module load python
 
 
-# activate it
-source myenv/bin/activate
-
 python3 benchmark.py
+
+echo "done"
